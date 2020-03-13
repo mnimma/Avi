b9f5740ce790a541843de502373536e417f28051
Nimma MadhavaReddy <61187692+mnimma@users.noreply.github.com>
2020-03-13 18:28:18 +0530

    Update hello.java

--- src/hello.java
+++ src/hello.java
@@ -3,13 +3,14 @@ class hello {
 	static int c = 30;
 
 	public static void main(String args[]) {
-		int a = 20;
+		int a = 20; int a1=30;
 		final int e = 40;
 		hello a1 = new hello();
 		System.out.println(a);
 		System.out.println(a1.b);
 		System.out.println(c);
 		System.out.println(e);
+		System.out.println(a1);
 
 	}
 }
